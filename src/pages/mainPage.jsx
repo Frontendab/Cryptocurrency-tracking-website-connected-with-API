7a07210ff57d9ae22d56b27ad1e0f826e4247e54
Ayoub Sadouri <135533101+Frontendab@users.noreply.github.com>
2023-11-30 19:04:04 +0100

    Update mainPage.jsx

--- src/pages/mainPage.jsx
+++ src/pages/mainPage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { createHashRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import { PAGE } from "./PAGE";
 import { FeaturedRoutes } from "../components/ROUTES/featuredRoutes";
 import { EarnRoutes } from "../components/ROUTES/earnRoutes";
